97e7dd57470bdf462e7095808510069a8c26378a
tanlye <tan_lovell@hotmail.com>
2024-10-28 21:56:20 +0800

    v.0.1.3 updated validate

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,16 +3,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    # Move archive provider from lambda.tf
-    archive = {
-      source  = "hashicorp/archive"
-      version = "~> 2.0"
+      version = ">= 5.73.0"
     }
   }
-  # Added by wtc - 2024-10-03
-  required_version = ">= 1.1.0"
 }
 
 # Configure the AWS Provider
